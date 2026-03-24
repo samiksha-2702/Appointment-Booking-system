932642eca79815a41789381e24d5f51e3228ed50
Samiksha Apaka <samikshaapake@gmail.com>
2026-03-24 15:09:15 +0530

    Resolved merge conflict

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    availability = models.TimeField()
+    available_to = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('Booked', 'Booking'),
+        ('Completed', 'Completed'),
+        ('Canclled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Booked')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.Doctor.name}"
